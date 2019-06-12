b806c83b9b5a7e4fba9ebb9b9592ea9036977e41
o6241 <51728785+o6241@users.noreply.github.com>
2019-06-12 15:27:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "서버 호스트 : {}".format(platform.node())
+    return "서버 호스트2 : {}".format(platform.node())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
